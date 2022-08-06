4ae93a363c65909f97859e95ac0f5b0b882bf550
rmflight <r.flight79@gmail.com>
2022-08-06 14:00:59 -0400

    and function stubs

---
+++ R/differential.R
@@ -0,0 +1,14 @@
+normalize_samples = function(lipid_measurements){
+  # do normalization in here and return the df
+  NULL
+}
+
+impute_missing = function(lipid_normalized){
+  # do imputation
+  NULL
+}
+
+differential_test = function(lipid_imputed){
+  # do differential test via limma
+  NULL
+}
