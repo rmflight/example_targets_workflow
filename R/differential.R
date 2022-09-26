d7edb180c824e87888f1a973f27804aff98352ee
rmflight <r.flight79@gmail.com>
2022-09-26 11:56:55 -0400

    adding some more files

--- R/differential.R
+++ R/differential.R
@@ -1,14 +1,58 @@
 normalize_samples = function(lipid_measurements){
   # do normalization in here and return the df
-  NULL
+  # tar_load(lipid_measurements)
+  numeric_only = lipid_measurements |>
+    dplyr::select(-feature_id, -class, -name, -group_units)
+  numeric_long = numeric_only |>
+    tidyr::pivot_longer(cols = everything(), names_to = "sample", values_to = "abundance")
+  med_vals = numeric_long |>
+    dplyr::filter(!is.na(abundance)) |>
+    dplyr::group_by(sample) |>
+    dplyr::summarise(median = median(abundance))
+  
+  norm_values = purrr::map_dfc(med_vals$sample, function(in_sample){
+    use_norm = med_vals |>
+      dplyr::filter(sample %in% in_sample) |>
+      dplyr::pull(median)
+    numeric_only[[in_sample]] / use_norm
+  })
+  names(norm_values) = med_vals$sample
+  
+  norm_values$feature_id = lipid_measurements$feature_id
+  norm_values
+  
 }
 
 impute_missing = function(lipid_normalized){
   # do imputation
-  NULL
+  # tar_load(lipid_normalized)
+  numeric_long = lipid_normalized |>
+    tidyr::pivot_longer(cols = -feature_id, names_to = "sample", values_to = "abundance")
+  log_dist = log(numeric_long$abundance)
+  min_log = min(log_dist, na.rm = TRUE)
+  impute_val = exp(min_log + (min_log * 0.1))
+  numeric_long$abundance[is.na(numeric_long$abundance)] = impute_val
+  
+  imputed_data = numeric_long |>
+    tidyr::pivot_wider(id_cols = "feature_id", names_from = "sample", values_from = "abundance")
+  imputed_data
 }
 
-differential_test = function(lipid_imputed){
+differential_test = function(lipid_imputed, lipid_metadata){
   # do differential test via limma
-  NULL
+  # tar_load(lipid_imputed)
+  # tar_load(lipid_metadata)
+  log_matrix = lipid_imputed |>
+    dplyr::select(all_of(lipid_metadata$sample_id)) |>
+    as.matrix() |>
+    log2()
+  rownames(log_matrix) = lipid_imputed$feature_id
+  
+  lipid_metadata$group_info = factor(lipid_metadata$group_units, levels = c("KO", "WT"), ordered = TRUE)
+  design_matrix = model.matrix(~ group_info, lipid_metadata)
+  log_fit = limma::lmFit(log_matrix, design = design_matrix)
+  log_fit = limma::eBayes(log_fit)
+  log_limma = limma::topTable(log_fit, coef = "group_info.L", number = Inf, p.value = 1, sort.by = "none")
+  log_limma$feature_id = rownames(log_matrix)
+  log_limma
 }
--- _targets.R
+++ _targets.R
@@ -23,23 +23,27 @@ tar_plan(
   # for most other types of targets outside of documents.
   lipid_measurements = readr::read_csv(measurement_file),
   lipid_metadata = readr::read_csv(metadata_file),
-  
-  # next we want to explore the data and evaluate if there is any funny 
+
+  # next we want to explore the data and evaluate if there is any funny
   # things we need to worry about.
   # I prefer to do that in an rmarkdown or quarto document.
   # Use tflow::use_rmd("document") to set this up.
   tar_render(exploration, "doc/exploration.Rmd"),
-  
+
   # next, we do sample normalization using median values.
   # We are going to use a function to do that.
   lipid_normalized = normalize_samples(lipid_measurements),
-  
+
   # and then a simple imputation method
   lipid_imputed = impute_missing(lipid_normalized),
-  
+
   # and then do differential analysis
-  lipids_differential = differential_test(lipid_imputed),
+  lipids_differential = differential_test(lipid_imputed, lipid_metadata),
   
+  tar_target(exploration_child,
+             "doc/exploration_child.Rmd",
+             format = "file")
+
   # and then generate final report
-  tar_render(differential_report, "doc/differential_report.Rmd")
+  # tar_render(differential_report, "doc/differential_report.Rmd")
 )
